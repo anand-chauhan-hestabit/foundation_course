88ae8812612886382a56595550321d3018be0662
anand-chauhan-hestabit <123088207+anand-chauhan-hestabit@users.noreply.github.com>
2023-01-24 16:45:27 +0530

    Update task3_file2.php
    Improve code acording to PSR.

--- task3_file2.php
+++ task3_file2.php
@@ -1,11 +1,16 @@
 <?php
+
 namespace second_namespace;
-class StringMagic{
-    public function split_string_array(String $user_string){
-       echo"split the string into an array of $user_string"." :";
-      print_r(explode(" ",$user_string));
+
+class StringMagic
+{
+    public function split_string_array(String $user_string)
+    {
+        echo"split the string into an array of $user_string"." :";
+        print_r(explode(" ",$user_string));
       
     }
   
     }
+
 ?>
