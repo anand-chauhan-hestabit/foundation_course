e5ec43281ee22438a3cdda5002a37581529a5683
anand-chauhan-hestabit <123088207+anand-chauhan-hestabit@users.noreply.github.com>
2023-01-24 15:54:29 +0530

    Update task2.3.php
    improve code acording to PSR.

--- task2.3.php
+++ task2.3.php
@@ -1,8 +1,9 @@
 <?php
-
 //create a traits
-trait getdata{
-    public $first_number,$second_number;
+trait getdata
+{
+    public $first_number;
+    public $second_number;
     public $result;
 
     //method to get a input
@@ -13,7 +14,9 @@ trait getdata{
     }
 }
 //class for devision
-class devide{
+class devide
+{
+
     use getdata;
     
     //method for devide
@@ -24,16 +27,20 @@ class devide{
     }
 }
 //class for basic addition and multiplications
-class calculator extends devide{
+class calculator extends devide
+{
+
     use getdata;
 
     //methods for add
-    function add(){
-         $this->result = $this->first_number + $this->second_number;
-         echo "Addition :".$this->result;
+    function add()
+    {
+        $this->result = $this->first_number + $this->second_number;
+        echo "Addition :".$this->result;
     }
-
-    function multiply(){
+    //methods for multiply
+    function multiply()
+    {
         $this->result = $this->first_number * $this->second_number;
         echo "Multiplication :".$this->result;
     }
@@ -42,11 +49,11 @@ class calculator extends devide{
 $object = new calculator();
 $object->getdata(100,11);
 echo "<h2>3. Create a calculator with the only Add, Multiply & Divide functionality by using
-the inheritance and using Trait.</h2>";
+      the inheritance and using Trait.</h2>";
 echo "First number is :".$object->first_number."<br>". "second number is :".$object->second_number."<br>";
-$object->add()
-;echo"<br>";
+$object->add();
+echo "<br>";
 $object->multiply();
-echo"<br>";
+echo "<br>";
 $object->division()
 ?>
