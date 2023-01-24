fa5ace64e372165e17a53c14acb04eec2485b420
anand-chauhan-hestabit <123088207+anand-chauhan-hestabit@users.noreply.github.com>
2023-01-24 16:25:26 +0530

    Update task3_file1.php
    Improve code acording to PSR.

--- task3_file1.php
+++ task3_file1.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace first_namespace;
-class StringMagic{
-    public function reverse_the_string(String $user_string)
+
+class StringMagic
+{
+    public function reverse_the_String(String $user_string)
     {
         $result = strrev($user_string);
-        echo"Reverse String of $user_string"."   :";
+        echo "Reverse String of $user_string"."   :";
         echo $result;
 
     }
