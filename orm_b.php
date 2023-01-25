d8d839344f5c3c1dd5a5d0eac1b3f2767516eb38
anand-chauhan-hestabit <123088207+anand-chauhan-hestabit@users.noreply.github.com>
2023-01-25 12:03:53 +0530

    Update orm_b.php
    Updated code change method calling and improve code acording to PSR...

--- orm_b.php
+++ orm_b.php
@@ -1,84 +1,88 @@
 <?php
-class Singleton{
-    private static $instance = NULL;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class DbConnect
+{   
+    private static $instance = null;
+    private static $conn;
+
     private function __construct(){
-        echo"<h2>Db connected</h2>";
-    }
-    public static function getInstance(){
-        if(self::$instance == NULL){
-            self::$instance = new Static();
-         }else{
-            echo"already connected <br>";
-         }
-         return self::$instance;
-    }
-    #DATABASE CONNECTION
-    public static function getDBConn(){
-        try{
-            $db = self::$instance;
-            $db->dbconn = new mysqli('localhost','hestabit','hestabit','anand');
-            return $db->dbconn;
-        }
-        catch(Exception $e){
-            echo "error:-".$e->getMessage();
-        }
+
+        self::$conn = new mysqli('localhost', 'hestabit', 'hestabit', 'anand');
+        echo "conected....<br>";
+
     }
 
-   #FETCH DATA USING GIVEN TABLE AND COLUMN NAME
-    public static function fetch_Data($table_name,$column_name){
-        $obj =  singleton::getDBConn();
-        $sql = "select $column_name from $table_name";
-        $result = $obj->query($sql);
-        $response = $result->fetch_all();
-        echo"<pre>";print_r($response);
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new DbConnect();
+            return self::$instance;
+        } else {
+             echo "now cant initiate instance.... ";
+            //die('now cant initiate instance.... ');
+        }
+        
     }
-    #INSERT DATA INTO DATABASE USING GIVEN TABLE AND COLUMN NAME
-    public static function insert_Into_Db($table_name,$column_name,$value){
-        $obj =  singleton::getDBConn();
+
+    public static function getConnection()
+    {  
+        return self::$conn;
+    }
+
+
+    #FETCH DATA USING GIVEN TABLE AND COLUMN NAME
+    public  function fetch_Data($table_name, $column_name)
+     {
+            $obj =  DbConnect::getConnection();
+            $sql = "select $column_name from $table_name";
+            $result = $obj->query($sql);
+            echo "<pre>";print_r($result->fetch_all());
+    }
+
+     #INSERT DATA INTO DATABASE USING GIVEN TABLE AND COLUMN NAME
+     public function insert_Into_Db($table_name, $column_name, $value){
+        $obj =  DbConnect::getConnection();
         $sql = "INSERT INTO `$table_name`($column_name) VALUES ('$value')";
         $result = $obj->query($sql);
-        if($result){
-            echo"inserted succesfully<br>";
-        }else{
-            echo"not inserted data";
+        if ($result) {
+            echo "inserted $value succesfully<br>";
+        } else {
+            echo "not inserted data";
         }
     }
 
-    #UPDATE ROW USING GIVEN TABLE, COLUMN NAME AND VALUES 
-    public static function update_row($table_name,$column_name,$old_Value,$new_Value){
-        $obj =  singleton::getDBConn();
+      #UPDATE ROW USING GIVEN TABLE, COLUMN NAME AND VALUES 
+      public static function update_row($table_name, $column_name, $old_Value, $new_Value){
+        $obj =  DbConnect::getConnection();
         $sql = "UPDATE `$table_name` SET $column_name = '$new_Value' WHERE $column_name='$old_Value'";
         $result = $obj->query($sql);
-        if($result){
-            echo"updated data succesfully<br>";
-        }else{
-            echo"not updated  data";
+        if ($result) {
+            echo "updated data $old_Value with $new_Value succesfully<br>";
+        } else { 
+            echo "not updated  data";
         }
     }
 
     #Delete row by providing a table name, column name, and value of the column.
-
-    public static function delete_row($table_name,$column_name,$value){
-        $obj = singleton::getDBConn();
+    public static function delete_row($table_name, $column_name, $value){
+        $obj =  DbConnect::getConnection();
         $sql = "DELETE FROM `$table_name` WHERE $column_name = '$value'";
         $result = $obj->query($sql);
-        if($result){
-            echo"Deleted $value successfully....<br>";
-        }else{
-            echo"not deleted";
+        if ($result) {
+            echo "Deleted $value successfully....<br>";
+        } else {
+            echo "not deleted";
         }
     }
-} echo"<pre>";
-echo"<h2>b. Need the following functions in the class:
-i.Connect DB (use singleton method for this)
-ii.Select data by providing a table name and columns that need to be fetched.
-iii.Insert into DB by providing a table name, column name, and values.
-iv.Update row by providing a table name, column name, and value.
-v.Delete row by providing a table name, column name, and value of the
-column";
-singleton::getInstance();
-singleton::insert_Into_Db("anandt","name","Vikas kumar");
-singleton::update_row("anandt","name","vikas kumar","Gorakhpur");
-singleton::delete_row("anandt","name","Rajesh kumar");
-singleton::fetch_Data("anandt","name");
-?>
+}
+
+$db = DbConnect::getInstance();
+$db->fetch_Data("anandt","name");
+$db->insert_Into_Db("anandt","name","Vikas kumar");
+$db->update_row("anandt","name","Vikas kumar","Ranjan kumar");
+$db->delete_row("anandt","name","Gorakhpur");
+
